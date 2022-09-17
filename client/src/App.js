24e97a075909d43d8a690ac6dc1e67fe7b9d1d84
Spencer Rafada <spencer.byui@gmail.com>
2022-09-16 22:48:20 -0600

    commit front-end react code

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:9000/testAPI")
+    .then((res) => res.json())
+    .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {!data ? "Uhm..." : data}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
