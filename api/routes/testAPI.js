652e8c13c7d56949ab1bf36c82e941e52987b4e9
Spencer Rafada <spencer.byui@gmail.com>
2022-09-26 19:40:01 -0600

    refactored whole structure

--- api/routes/testAPI.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get("/", function(req, res, next){
-    console.log("GET request received")
-    res.json({ message: "U GOT THIS!" });
-});
-
-module.exports = router;
