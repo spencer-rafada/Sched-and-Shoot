06f40397621f6615c499904513f723046b719106
Spencer Rafada <spencer.byui@gmail.com>
2022-10-04 23:08:41 -0600

    fixed bug

--- server/controllers/photographers.js
+++ server/controllers/photographers.js
@@ -1,46 +1,44 @@
-import Photographer from '../models/photographerSchema.js';
-import { MongoClient } from 'mongodb';
+import Photographer from "../models/photographerSchema.js";
+import { MongoClient } from "mongodb";
 
-const URL = 'mongodb+srv://snaptester02:C9po8wNAeMIiz3v1@oh-snap.99pknxd.mongodb.net/?retryWrites=true&w=majority';
+const URL =
+  "mongodb+srv://snaptester02:C9po8wNAeMIiz3v1@oh-snap.99pknxd.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(URL);
 const db = client.db("test");
 const coll = db.collection("photographers");
 
-const cursor = coll.find();
-
 // Turn this to async as well
 export const getPhotographers = async (req, res) => {
-    try {
-        await client.connect(); 
+  try {
+    await client.connect();
 
+    const cursor = coll.find();
 
-        // takes the [0] of the array
-        const data = await cursor.toArray();
+    // takes the [0] of the array
+    const data = await cursor.toArray();
 
-        res.json(data);
-
-    } finally {
-        await client.close();
-    }
-    console.log("GET")
-}
+    res.json(data);
+  } finally {
+    await client.close();
+  }
+  console.log("GET");
+};
 
 export const createPhotographer = async (req, res) => {
-    try {
-        await client.connect();
+  try {
+    await client.connect();
 
-        console.log(req.body);
-        const result = await coll.insertMany(req.body);
-        console.log(result.insertedIds);
+    console.log(req.body);
+    const result = await coll.insertMany(req.body);
+    console.log(result.insertedIds);
+  } finally {
+    await client.close();
+  }
 
-    } finally {
-        await client.close();
-    }
-
-    console.log("POST");
-}
+  console.log("POST");
+};
 
 export const deletePhotographer = (req, res) => {
-    res.json({message: "DELETE new photographer"})
-}
+  res.json({ message: "DELETE new photographer" });
+};
